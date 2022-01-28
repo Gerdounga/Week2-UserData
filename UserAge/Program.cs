5ecd9ce0295daa2a3681a288c3270c636e6d8a25
Gerdo Unga <gerdo.unga@gmail.com>
2022-01-28 20:42:05 +0200

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis;
+            //''Oled sündinud aastal {yearOfBirth}";
+
+            Console.WriteLine("Palun, sisesta oma vanus:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2022 - yearOfBirth;
+
+            //Console.WriteLine("Oled sündinud " + userAge + " aastal.");
+
+            //või teine variant
+            //string interpolation
+
+            Console.WriteLine($"Oled sündinud {userAge} aastal.");
+
+        }
+    }
+}
